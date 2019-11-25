0e59be3c88435b1b53db7a5954b3ac2e28f9793b
Ben Ryan <brryan@lanl.gov>
2019-11-25 11:47:56 -0700

    Got all the units in

--- src/units/ConstexprUnitSystem.hh
+++ src/units/ConstexprUnitSystem.hh
@@ -12,131 +12,244 @@ struct SI {
 };
 
 struct CGS {
-  static constexpr double Length = 1.e2;    // centimeter
-  static constexpr double Mass = 1.e3;      // gram
-  static constexpr double Time = 1.;        // second
-  static constexpr double Temperature = 1.; // Kelvin
-  static constexpr double Current = 1.e-1;  // Biot
-  static constexpr double Charge = 2.997924580e9 // Statcoulomb
+  static constexpr double Length = 1.e2;                    // centimeter
+  static constexpr double Mass = 1.e3;                      // gram
+  static constexpr double Time = 1.;                        // second
+  static constexpr double Temperature = 1.;                 // Kelvin
+  static constexpr double Current = 1.e-1;                  // Biot
+  static constexpr double Charge = 2.997924580e9;           // Statcoulomb
   static constexpr double Capacitance = 8.9831483395497e11; // Statfarad
-  static constexpr double Angle = 1.;       // Radian
-  static constexpr double Quantity = 1.;    // Mole
+  static constexpr double Angle = 1.;                       // Radian
+  static constexpr double Quantity = 1.;                    // Mole
 };
 
 struct CGSH {
-  static constexpr double Length = 1.e2;                  // centimeter
-  static constexpr double Mass = 1.e3;                    // gram
-  static constexpr double Time = 1.e8;                    // shake
-  static constexpr double Temperature = 8.61733238496e-8; // Kelvin
-  static constexpr double Current = 1.e-1;                // Biot
-  static constexpr double Charge = 2.997924580e9 // Statcoulomb
+  static constexpr double Length = 1.e2;                    // centimeter
+  static constexpr double Mass = 1.e3;                      // gram
+  static constexpr double Time = 1.e8;                      // shake
+  static constexpr double Temperature = 8.61733238496e-8;   // Kelvin
+  static constexpr double Current = 1.e-1;                  // Biot
+  static constexpr double Charge = 2.997924580e9;           // Statcoulomb
   static constexpr double Capacitance = 8.9831483395497e11; // Statfarad
-  static constexpr double Angle = 1.;                     // Radian
-  static constexpr double Quantity = 1.;                  // Mole
+  static constexpr double Angle = 1.;                       // Radian
+  static constexpr double Quantity = 1.;                    // Mole
 };
 
-template <typename UNITS>
-class PhysicalConstexprs {
+template <typename UNITS> class PhysicalConstexprs {
 public:
   constexpr PhysicalConstexprs() {}
 
   //! accesses Avogadro's number
   constexpr double avogadro() const { return d_avogadro; }
+  //! see avogadro()
   constexpr double Na() const { return avogadro(); }
 
   //! accesses the fine structure constant
   constexpr double fineStructure() const { return d_fineStructure; }
+  //! see fineStructure()
   constexpr double alpha() const { return fineStructure(); }
 
   //! accesses Planck's constant
   constexpr double planck() const { return d_planck; }
+  //! see planck()
   constexpr double h() const { return planck(); }
 
   //! accesses the molar gas constant
   constexpr double gasConstant() const { return d_gasConstant; }
+  //! see gasConstant()
   constexpr double R() const { return gasConstant(); }
 
   //! accesses the Boltzmann constant
   constexpr double boltzmann() const { return d_boltzmann; }
+  //! see boltzmann()
   constexpr double k() const { return boltzmann(); }
 
   //! accesses the electron charge
   constexpr double electronCharge() const { return d_electronCharge; }
+  //! see electronCharge()
   constexpr double e() const { return electronCharge(); }
 
   //! accesses the speed of light
   constexpr double speedOfLight() const { return d_speed_of_light_SI; }
+  //! see speedOfLight()
   constexpr double c() const { return d_speed_of_light; }
 
   //! accesses the Stefan-Boltzmann constant
   constexpr double stefanBoltzmann() const { return d_stefanBoltzmann; }
+  //! see stefanBoltzmann()
   constexpr double sigma() const { return stefanBoltzmann(); }
 
   //! accesses the gravitational constant
-  constexpr double gravitationalConstant() const { return d_gravitationalConstant; }
-  constexpr double G() const 
+  constexpr double gravitationalConstant() const {
+    return d_gravitationalConstant;
+  }
+  //! see gravitationalConstant()
+  constexpr double G() const { return gravitationalConstant(); }
+
+  //! accesses the standard acceleration of gravity
+  constexpr double accelerationFromGravity() const {
+    return d_accelerationFromGravity;
+  }
+  //! see accelerationFromGravity()
+  constexpr double g() const { return accelerationFromGravity(); }
+
+  //! accesses the Faraday constant
+  constexpr double faradayConstant() const { return d_faradayConstant; }
+  //! see faradayConstant()
+  constexpr double F() const { return faradayConstant(); }
+
+  //! access the permeability of free space
+  constexpr double permeabilityOfVacuum() const {
+    return d_permeabilityOfVacuum;
+  }
+  //! see permeabilityOfVacuum()
+  constexpr double mu0() const { return permeabilityOfVacuum(); }
+
+  //! access the permittivity of free space
+  constexpr double permittivityOfFreeSpace() const {
+    return d_permittivityOfVacuum;
+  }
+  //! see permittivityOfFreeSpace()
+  constexpr double epsi0() const { return permittivityOfFreeSpace(); }
+
+  //! access the classical electron radius
+  constexpr double classicalElectronRadius() const {
+    return d_classicalElectronRadius;
+  }
+  //! see classicalElectronRadius()
+  constexpr double re() const { return classicalElectronRadius(); }
+
+  //! access the electron mass
+  constexpr double electronMass() const { return d_electronMass; }
+  //! see electronMass()
+  constexpr double me() const { return electronMass(); }
+
+  //! access the proton mass
+  constexpr double protonMass() const { return d_protonMass; }
+  //! see protonMass()
+  constexpr double mp() const { return protonMass(); }
 
 private:
   // Derived unit conversions
-  static constexpr double Force = UNITS::Mass*UNITS::Length/(UNITS::Time*UNITS::Time);
-  static constexpr double Energy = Force*UNITS::Length;
+  static constexpr double Length = UNITS::Length;
+  static constexpr double Mass = UNITS::Mass;
+  static constexpr double Time = UNITS::Time;
+  static constexpr double Temperature = UNITS::Temperature;
+  static constexpr double Current = UNITS::Current;
+  static constexpr double Charge = UNITS::Charge;
+  static constexpr double Capacitance = UNITS::Capacitance;
+  static constexpr double Angle = UNITS::Angle;
+  static constexpr double Quantity = UNITS::Quantity;
+  static constexpr double Force = Mass * Length / (Time * Time);
+  static constexpr double Energy = Force * Length;
+  static constexpr double Power = Energy / Time;
 
   // Dimensionless constants
   //! Avogadro cosntant (CODATA 2018 exact value)
   static constexpr double d_avogadro = 6.02214076e23;
-  
+
   //! Fine structure constant (CODATA 2018 value)
-  static constexpr double d_finestructure = 7.2973525693e-3;
+  static constexpr double d_fineStructure = 7.2973525693e-3;
 
   // SI values of fundamental constants
   //! Planck constant (CODATA 2018 exact value)
-  static constexpr double d_planck_SI = 6.62607015e-34;       // J s
-  
+  static constexpr double d_planck_SI = 6.62607015e-34; // J s
+
   //! Molar gas constant (CODATA 2018 exact value up to precision)
-  static constexpr double d_gasConstant = 8.3144621618;          // J mol^-1 K^-1
-  
+  static constexpr double d_gasConstant_SI = 8.3144621618; // J mol^-1 K^-1
+
   //! Boltzmann constant (CODATA 2018 exact value)
-  static constexpr double d_boltzmann = 1.380649e-23;     // J K^-1
-  
+  static constexpr double d_boltzmann_SI = 1.380649e-23; // J K^-1
+
   //! Electron charge (CODATA 2018 exact value)
-  static constexpr double d_electronCharge = 1.602176634e-19; // C
-  
+  static constexpr double d_electronCharge_SI = 1.602176634e-19; // C
+
   //! Speed of light (CODATA 2018 exact value)
   static constexpr double d_speed_of_light_SI = 2.99792458e8; // m s^-1
-  
+
   //! Stefan-Boltzmann constant (CODATA 2018 exact value up to precision)
-  static constexpr double d_stefanBoltzmann = 5.670374419e-8; // W m^-2 K^-4
-  
+  static constexpr double d_stefanBoltzmann_SI = 5.670374419e-8; // W m^-2 K^-4
+
   //! Gravitational constant (CODATA 2018 value)
-  static constexpr double d_gravitationalConstant = 6.67430e-11; // m^3 kg^-1 s^-2
-  
+  static constexpr double d_gravitationalConstant_SI =
+      6.67430e-11; // m^3 kg^-1 s^-2
+
   //! Standard acceleration of gravity (CODATA 2018 exact value)
-  static constexpr d_accelerationFromGravity = 9.80665; // m s^-2
-  
+  static constexpr double d_accelerationFromGravity_SI = 9.80665; // m s^-2
+
   //! Faraday constant (CODATA 2018 exact value up to precision)
-  static constexpr d_faradayConstant = 96485.33212; // C mol^-1
-  
-  //! Permeability of free space (CODATA 2018 value) 
-  static constexpr d_permeabilityOfVacuum = 1.25663706212e-6; // N A^-2
-  
+  static constexpr double d_faradayConstant_SI = 96485.33212; // C mol^-1
+
+  //! Permeability of free space (CODATA 2018 value)
+  static constexpr double d_permeabilityOfVacuum_SI =
+      1.25663706212e-6; // N A^-2
+
   //! Permittivity of free space (CODATA 2018 value)
-  static constexpr d_permittivityOfVacuum = 8.8541878128e-12; // F m^-1
-  
+  static constexpr double d_permittivityOfVacuum_SI =
+      8.8541878128e-12; // F m^-1
+
   //! Classical electron radius (CODATA 2018 value)
-  static constexpr d_classicalElectronRadius = 2.8179403262e10-15; // m
-  
+  static constexpr double d_classicalElectronRadius_SI =
+      2.8179403262e-15; // m
+
   //! Electron rest mass (CODATA 2018 value)
-  static constexpr d_electronMass = 9.1093837015e-31; // kg
-  
+  static constexpr double d_electronMass_SI = 9.1093837015e-31; // kg
+
   //! Proton rest mass (CODATA 2018 value)
-  static constexpr d_protonMass = 1.67262192369e-27; // kg
+  static constexpr double d_protonMass_SI = 1.67262192369e-27; // kg
 
+  // Dimensional values converted to templated unit system
+  //! Planck constant value in unit system
+  static constexpr double d_planck = d_planck_SI * Energy * Time;
 
-  // Values converted to templated unit system
-  //static constexpr double d_planck =
-  //    d_planck_SI *UNITS::Mass*pow(UNITS::Length,2)/UNITS::Time;
-  static constexpr double d_planck = d_planck_SI*Energy*UNITS::Time;
+  //! Molar gas constant value in unit system
+  static constexpr double d_gasConstant =
+      d_gasConstant_SI * Energy / Quantity / Temperature;
+
+  //! Boltzmann constant value in unit system
+  static constexpr double d_boltzmann = d_boltzmann_SI * Energy / Temperature;
+
+  //! Electron charge value in unit system
+  static constexpr double d_electronCharge = d_electronCharge_SI * Charge;
+
+  //! Speed of light value in unit system
   static constexpr double d_speed_of_light =
-          d_speed_of_light_SI * UNITS::Length / UNITS::Time;
+      d_speed_of_light_SI * Length / Time;
+
+  //! Stefan-Boltzmann constant value in unit system
+  static constexpr double d_stefanBoltzmann =
+      d_stefanBoltzmann_SI * Power /
+      (Length * Length * Temperature * Temperature * Temperature * Temperature);
+
+  //! Gravitational constant value in unit system
+  static constexpr double d_gravitationalConstant = d_gravitationalConstant_SI *
+                                                    Length * Length * Length /
+                                                    (Mass * Time * Time);
+
+  //! Standard acceleration of gravity value in unit system
+  static constexpr double d_accelerationFromGravity =
+      d_accelerationFromGravity_SI * Length / (Time * Time);
+
+  //! Faraday constant value in unit system
+  static constexpr double d_faradayConstant =
+      d_faradayConstant_SI * Capacitance;
+
+  //! Permeability of vacuum value in unit system
+  static constexpr double d_permeabilityOfVacuum =
+      d_permeabilityOfVacuum_SI * Force / (Current * Current);
+
+  //! Permittivity of vacuum value in unit system
+  static constexpr double d_permittivityOfVacuum =
+      d_permittivityOfVacuum_SI * Capacitance / Length;
+
+  //! Classical electron radius value in unit system
+  static constexpr double d_classicalElectronRadius =
+      d_classicalElectronRadius_SI * Length;
+
+  //! Electron mass value in unit system
+  static constexpr double d_electronMass = d_electronMass_SI * Mass;
+
+  //! Proton mass value in unit system
+  static constexpr double d_protonMass = d_protonMass_SI * Mass;
 };
-} // namespace rtt_constunits
+} // namespace rtt_units
