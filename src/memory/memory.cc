413b139bbd78a99703b8ee44c6d51f838b5d50ba
Kelly (KT) Thompson <kgt@lanl.gov>
2019-12-28 13:55:03 -0700

    Fix missing ifdef to protect clang specific pragma.

--- src/memory/memory.cc
+++ src/memory/memory.cc
@@ -14,12 +14,18 @@
 #include <limits>
 #include <map>
 
+#ifdef __clang__
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wreserved-id-macro"
+#endif
+
 #ifndef _GLIBCXX_THROW
 #define _GLIBCXX_THROW(except) throw(except)
 #endif
+
+#ifdef __clang__
 #pragma clang diagnostic pop
+#endif
 
 namespace rtt_memory {
 using namespace std;
