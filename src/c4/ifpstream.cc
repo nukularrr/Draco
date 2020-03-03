3a29e6e1eeff5ae51492c39c8c90a936f6e3ec8e
Mathew Cleveland <cleveland@lanl.gov>
2020-03-03 10:07:27 -0700

    Add an Insist that the file exists

--- src/c4/ifpstream.cc
+++ src/c4/ifpstream.cc
@@ -32,6 +32,7 @@ ifpstream::ifpstream(std::string const &filename,
   mode_ = mode;
   if (rtt_c4::node() == 0) {
     in_.open(filename, mode);
+    Insist(in_, filename + " does not exist");
   }
 }
 
