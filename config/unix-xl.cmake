afea3ae31cf9fe49a3536671db6a76dd6475e4a3
Kelly (KT) Thompson <kgt@lanl.gov>
2020-05-11 14:40:04 -0600

    Update XL compiler options for Darwin xl-16.1.1.X.

--- config/unix-xl.cmake
+++ config/unix-xl.cmake
@@ -1,6 +1,6 @@
 #-----------------------------*-cmake-*----------------------------------------#
 # file   config/unix-xl.cmake
-# author Gabriel Rockefeller
+# author Gabriel Rockefeller, Kelly Thompson <kgt@lanl.gov>
 # date   2012 Nov 1
 # brief  Establish flags for Linux64 - IBM XL C++
 # note   Copyright (C) 2016-2020 Triad National Security, LLC.
@@ -22,18 +22,57 @@ query_openmp_availability()
 if( NOT CXX_FLAGS_INITIALIZED )
    set( CXX_FLAGS_INITIALIZED "yes" CACHE INTERNAL "using draco settings." )
 
-  # On Darwin, we also need this config file:
-  # -F/projects/opt/ppc64le/ibm/xlc-16.1.1.2/xlC/16.1.1/etc/xlc.cfg.rhel.7.5.gcc.7.3.0.cuda.9.2
-  # -qfloat=nomaf -qxlcompatmacros
   set( CMAKE_C_FLAGS             "-g" )
-  # ATS-2
   if( EXISTS /usr/gapps )
+    # ATS-2
     string( APPEND CMAKE_C_FLAGS " --gcc-toolchain=/usr/tce/packages/gcc/gcc-8.3.1" )
+  elseif(EXISTS /projects/opt/ppc64le/ibm )
+    # Darwin power9 - extract version from module environment.
+    string(REPLACE ":" ";" modules $ENV{LOADEDMODULES})
+    foreach( module ${modules} )
+      if( ${module} MATCHES "^gcc" )
+        string( REGEX REPLACE
+          "[^0-9]*([0-9]+).([0-9]+).([0-9]+)" "\\1.\\2.\\3"
+          gcc_version  ${module} )
+      elseif( NOT DEFINED xlc_version AND ${module} MATCHES "^ibm/xlc" )
+        string( REGEX REPLACE
+          "[^0-9]*([0-9]+).([0-9]+).([0-9]+).([0-9]+).*" "\\1.\\2.\\3.\\4"
+          xlc_version ${module} )
+        string( REGEX REPLACE "[^0-9]*([0-9]+).([0-9]+).([0-9]+).*" "\\1.\\2.\\3"
+          xlc_version_3 ${xlc_version} )
+#      Only cuda-10.1 is currently supported
+      elseif( ${module} MATCHES "^cuda" )
+        string( REGEX REPLACE
+          "[^0-9]*([0-9]+).([0-9]+)" "\\1.\\2"
+          cuda_version ${module} )
+        if( NOT ${cuda_version} STREQUAL "10.1" )
+          message( FATAL_ERROR "Only cuda/10.1 is currently supported."
+            " Found cuda/${cuda_version} in your environment. Replace your cuda"
+            " module with cuda/10.1 or unload the cuda module.")
+        endif()
+      endif()
+    endforeach()
+    # Only redhat 7.7 is currently supported
+    # file( READ /etc/redhat-release rhr )
+    #string( REGEX REPLACE "[^0-9]*([0-9]+).([0-9]+).*" "\\1.\\2"
+    #   redhat_version  ${rhr} )
+    set( config_file "/projects/opt/ppc64le/ibm/xlc-${xlc_version}/xlC/${xlc_version_3}/etc/xlc.cfg.rhel.7.7.gcc.${gcc_version}.cuda.10.1" )
+    if( EXISTS ${config_file} )
+      string( APPEND CMAKE_C_FLAGS " -F${config_file}" )
+    else()
+      message( FATAL_ERROR
+        "IBM XLC selected (Darwin), but requested config file was not found."
+        "\nconfig_file = ${config_file}" )
+    endif()
+    unset( gcc_version )
+    unset( xl_version )
+    unset( config_file )
   endif()
+
   # 2019-04-03 IBM support asks that we not use '-qcheck' due to compiler issues.
-  set( CMAKE_C_FLAGS_DEBUG          "-O0 -qsmp=omp:noopt -qfullpath -DDEBUG") # -qcheck -qoffload
+  set( CMAKE_C_FLAGS_DEBUG          "-O0 -qsmp=omp:noopt -qfullpath -DDEBUG")
   set( CMAKE_C_FLAGS_RELWITHDEBINFO
-    "-O3 -qhot=novector -qsmp=omp -qstrict=nans:operationprecision" ) # -qsimd=auto
+    "-O3 -qhot=novector -qsmp=omp -qstrict=nans:operationprecision" )
   set( CMAKE_C_FLAGS_RELEASE        "${CMAKE_C_FLAGS_RELWITHDEBINFO} -DNDEBUG" )
   set( CMAKE_C_FLAGS_MINSIZEREL     "${CMAKE_C_FLAGS_RELEASE}" )
 
--- config/unix-xlf.cmake
+++ config/unix-xlf.cmake
@@ -1,6 +1,6 @@
 #-----------------------------*-cmake-*----------------------------------------#
 # file   config/unix-xlf.cmake
-# author Gabriel Rockefeller
+# author Gabriel Rockefeller, Kelly Thompson <kgt@lanl.gov>
 # date   2012 Nov 1
 # brief  Establish flags for Unix - IBM XL Fortran
 # note   Copyright (C) 2016-2020 Triad National Security, LLC.
@@ -11,18 +11,24 @@
 # this macro.
 set( CMAKE_Fortran_COMPILER_FLAVOR "XL" )
 
+# -qsuppress=
+#   1501-210 (W) command option t contains an incorrect subargument
+#            This is related to '-pthread' showing up in the flags (a cmake bug)
+
 if( NOT Fortran_FLAGS_INITIALIZED )
    set( Fortran_FLAGS_INITIALIZED "yes" CACHE INTERNAL "using draco settings." )
    set( CMAKE_Fortran_FLAGS
-     "-qlanglvl=2008std -qinfo=all -qflag=i:w -qarch=auto" )
-   # IBM asked us to remove '-qcheck' due to compiler issues.
-   # -qsmp=noauto -qcheck
-   set( CMAKE_Fortran_FLAGS_DEBUG          "-O0 -qcheck" )
+     "-qlanglvl=2008std -qflag=i:w -qsuppress=1501-210" )
+   if( ${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "ppc64le")
+     string( APPEND CMAKE_Fortran_FLAGS " -qarch=pwr9 -qtune=pwr9" )
+   endif()
+
+   set( CMAKE_Fortran_FLAGS_DEBUG "-O0" )
    set( CMAKE_Fortran_FLAGS_RELWITHDEBINFO
-     "-O3 -qhot=novector -qsimd=auto -qstrict=nans:operationprecision" )
+     "-O3 -qstrict=nans:operationprecision" )
    set( CMAKE_Fortran_FLAGS_RELEASE
      "${CMAKE_Fortran_FLAGS_RELWITHDEBINFO}" )
-   set( CMAKE_Fortran_FLAGS_MINSIZEREL     "${CMAKE_Fortran_FLAGS_RELEASE}" )
+   set( CMAKE_Fortran_FLAGS_MINSIZEREL "${CMAKE_Fortran_FLAGS_RELEASE}" )
 endif()
 
 ##---------------------------------------------------------------------------##
@@ -38,45 +44,6 @@ set( CMAKE_Fortran_FLAGS_MINSIZEREL     "${CMAKE_Fortran_FLAGS_MINSIZEREL}"
   CACHE STRING "compiler flags" FORCE )
 set( CMAKE_Fortran_FLAGS_RELWITHDEBINFO "${CMAKE_Fortran_FLAGS_RELWITHDEBINFO}"
   CACHE STRING "compiler flags" FORCE )
-
-#
-# Toggle compiler flags for optional features
-#
-#toggle_compiler_flag( OPENMP_FOUND ${OpenMP_Fortran_FLAGS} "Fortran" "" )
-#toggle_compiler_flag( OPENMP_FOUND "-qsmp=noauto" "Fortran" "" )
-
-# ----------------------------------------------------------------------------
-# Helper macro to fix the value of
-# CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES when compiling on SQ with
-# xlc/xlf.  For some reason CMake is adding xlomp_ser to the link line
-# when a C++ main is built by linking to F90 libraries.  This library
-# should not be on For some reason CMake is adding xlomp_ser to the
-# link line when a C++ main the link line and actually causes the link
-# to fail on SQ.
-# ----------------------------------------------------------------------------
-# macro( remove_lib_from_link lib_for_removal )
-
-#    set( tmp "" )
-#    foreach( lib ${CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES} )
-#       if( ${lib} MATCHES ${lib_for_removal} )
-#          message("Removing ${lib_for_removal} from CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES")
-#          # do not add ${lib_for_removal} to the list ${tmp}.
-#       else()
-#          # Add a libraries except for the above match to the new list.
-#          list( APPEND tmp ${lib} )
-#       endif()
-#    endforeach()
-
-#    # Ensure that the value is saved to CMakeCache.txt
-#    set( CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES "${tmp}" CACHE STRING
-#       "Extra Fortran libraries needed when linking to Fortral library from C++ target"
-#       FORCE )
-
-# endmacro()
-
-# When building on SQ with XLC/XLF90, ensure that libxlomp_ser is not
-# on the link line
-#remove_lib_from_link( "xlomp_ser" )
 
 #------------------------------------------------------------------------------#
 # End config/unix-xlf.cmake
