a3f94e05e7bf53ffb3819f008e725458f59bcb11
Kelly (KT) Thompson <kgt@lanl.gov>
2020-05-31 17:21:36 -0600

    Update release scripts for Darwin.

--- tools/darwin-arm-env.sh
+++ tools/darwin-arm-env.sh
@@ -19,20 +19,25 @@ environments="armgcc820env"
 case $ddir in
 
   #---------------------------------------------------------------------------#
-  draco-7_2* | draco-7_3* | draco-7_4* | draco-7_5* | draco-7_6*)
+  draco-7_2* | draco-7_3* | draco-7_4* | draco-7_5* | draco-7_6* | draco-7_7*)
     function armgcc820env()
     {
       export darwin_queue="-p arm"
+      run "module list"
       run "module purge"
+      echo "VENDOR_DIR = ${VENDOR_DIR}"
+      echo "DRACO_ARCH = ${DRACO_ARCH}"
+      module use --append /opt/arm/modulefiles
+      module use --append /projects/darwin/modulefiles/aarch64
       module use --append ${VENDOR_DIR}/user_contrib
       module use --append ${VENDOR_DIR}-ec/Modules/$DRACO_ARCH
-      module load user_contrib
+      run "module load user_contrib"
 
       cflavor="gcc-8.2.0"
       mflavor="$cflavor-openmpi-3.1.3"
       lapackflavor="lapack-3.8.0"
       noflavor="git gcc/8.2.0 ndi"
-      compflavor="cmake/3.14.2-$cflavor gsl/2.5-$cflavor netlib-lapack/3.8.0-$cflavor numdiff/5.9.0-$cflavor random123/1.09-$cflavor metis/5.1.0-$cflavor eospac/6.4.0-$cflavor openmpi/3.1.3-gcc_8.2.0"
+      compflavor="cmake/3.17.0-$cflavor gsl/2.5-$cflavor netlib-lapack/3.8.0-$cflavor numdiff/5.9.0-$cflavor random123/1.09-$cflavor metis/5.1.0-$cflavor eospac/6.4.0-$cflavor openmpi/3.1.3-gcc_8.2.0"
       mpiflavor="libquo/1.3-$mflavor parmetis/4.0.3-$mflavor superlu-dist/5.2.2-$mflavor-$lapackflavor trilinos/12.14.1-$mflavor-$lapackflavor"
       ec_mf="ndi/2.1.3-$cflavor csk/0.5.0-$cflavor"
 
