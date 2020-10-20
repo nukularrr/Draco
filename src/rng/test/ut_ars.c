a11778050c63e14082f79cc9c993c066cb14c9f1
Kelly (KT) Thompson <KineticTheory@users.noreply.github.com>
2020-10-20 16:52:25 -0600

    Suppress an MSVC warning (#932)
    + Attempting to fix a dashboard issue.

--- src/rng/test/ut_ars.c
+++ src/rng/test/ut_ars.c
@@ -32,13 +32,13 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "rng/config.h"
 
 #ifdef _MSC_FULL_VER
-// - 4521: Engines have multiple copy constructors, quite legal C++, disable
-//         MSVC complaint.
+// - 4521: Engines have multiple copy constructors, quite legal C++, disable MSVC complaint.
 // - 4244: possible loss of data when converting between int types.
 // - 4204: nonstandard extension used - non-constant aggregate initializer
 // - 4127: conditional expression is constant
+// - 5105: macro expansion producing 'defined' has undefined behavior
 #pragma warning(push)
-#pragma warning(disable : 4521 4244 4127)
+#pragma warning(disable : 4521 4244 4127 5105)
 #endif
 
 #include "ut_ars.h"
