c83469e1977a0fb9547d64a8a5135686765dc3a2
Kelly (KT) Thompson <KineticTheory@users.noreply.github.com>
2021-02-24 08:09:10 -0700

    Here is another try at providing an mca orte_tmpdir_base hack for broken OpenMPI. (#1032)
    + The previous try used the test name as a unique identifier, but this created stings that were too
      long to be used as Unix domain sockets (error reported by OpenMPI).
    + Instead of using the test name, use an integer that is incremented for each registered test.

--- config/component_macros.cmake
+++ config/component_macros.cmake
@@ -558,8 +558,13 @@ macro( register_parallel_test )
   # Attempt of fix issues on Darwin related to /tmp permission errors, #2359.
   if( DEFINED ENV{SLURM_CLUSTER_NAME} AND "$ENV{SLURM_CLUSTER_NAME}" STREQUAL "darwin" AND
       MPI_FLAVOR STREQUAL "openmpi" AND NOT "${MPIEXEC_EXECUTABLE}" MATCHES "smpi")
+    if( NOT DEFINED orte_tmpdir_base_enum )
+      set( orte_tmpdir_base_enum 0 CACHE INTERNAL "help openmpi")
+    else()
+      math(EXPR orte_tmpdir_base_enum "${orte_tmpdir_base_enum} + 1")
+    endif()
     set( MPIEXEC_EXTRA_OPTS --mca orte_tmpdir_base
-      /tmp/$ENV{SLURMD_NODENAME}-$ENV{USER}-${rpt_TARGET} )
+      /tmp/$ENV{SLURMD_NODENAME}-$ENV{USER}-${orte_tmpdir_base_enum} ) # ${rpt_TARGET} )
   endif()
 
   if( addparalleltest_MPI_PLUS_OMP )
