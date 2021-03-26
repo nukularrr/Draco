185a6c7318a97c41a28122625d572aa9380be446
Kelly (KT) Thompson <KineticTheory@users.noreply.github.com>
2021-03-26 08:57:24 -0600

    Fix INTERNAL variable state logic. (#1045)

--- config/component_macros.cmake
+++ config/component_macros.cmake
@@ -558,12 +558,13 @@ macro( register_parallel_test )
   if( DEFINED ENV{SLURM_CLUSTER_NAME} AND "$ENV{SLURM_CLUSTER_NAME}" STREQUAL "darwin" AND
       MPI_FLAVOR STREQUAL "openmpi" AND NOT "${MPIEXEC_EXECUTABLE}" MATCHES "smpi")
     if( NOT DEFINED orte_tmpdir_base_enum )
-      set( orte_tmpdir_base_enum 0 CACHE INTERNAL "help openmpi")
+      set( orte_tmpdir_base_enum 0 )
     else()
       math(EXPR orte_tmpdir_base_enum "${orte_tmpdir_base_enum} + 1")
     endif()
+    set( orte_tmpdir_base_enum ${orte_tmpdir_base_enum} CACHE INTERNAL "help openmpi")
     set( MPIEXEC_EXTRA_OPTS --mca orte_tmpdir_base
-      /tmp/$ENV{SLURMD_NODENAME}-$ENV{USER}-${orte_tmpdir_base_enum} ) # ${rpt_TARGET} )
+      /tmp/$ENV{SLURMD_NODENAME}-$ENV{USER}-${orte_tmpdir_base_enum} )
   endif()
 
   if( addparalleltest_MPI_PLUS_OMP )
