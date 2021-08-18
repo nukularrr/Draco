79858594b0acdb6ec25fbdbdf70ee1ef60e49a97
Mathew Cleveland <clevelam@lanl.gov>
2021-08-18 11:37:59 -0600

    Adding const to lambdas which I think helps the compiler inline them (#1113)
    Co-authored-by: Mathew Cleveland <cleveland@lanl.gov>

--- src/kde/kde.i.hh
+++ src/kde/kde.i.hh
@@ -72,20 +72,20 @@ inline double kde::log_inv_transform(const double log_value, const double bias) 
 }
 
 //! Lambda to calculate a vector
-auto calc_vec = [](const auto &v1, const auto &v2) {
+const auto calc_vec = [](const auto &v1, const auto &v2) {
   Require(v1.size() == 3);
   Require(v2.size() == 3);
   return std::array<double, 3>{v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2]};
 };
 
 //! Lambda to calculate vector magnitude
-auto calc_mag = [](const auto &v) {
+const auto calc_mag = [](const auto &v) {
   Require(v.size() == 3);
   return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 };
 
 //! Lambda to calculate unit vector
-auto calc_unit_vec = [](const auto &v) {
+const auto calc_unit_vec = [](const auto &v) {
   Require(v.size() == 3);
   const double mag = calc_mag(v);
   return std::array<double, 3>{v[0] / mag, v[1] / mag, v[2] / mag};
